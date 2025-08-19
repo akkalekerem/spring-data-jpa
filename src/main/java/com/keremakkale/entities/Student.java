16afa268a7afa5977099a3b1e9522e158cf2729c
Kerem Akkale <keremakkale@gmail.com>
2025-08-19 14:42:47 +0300

    Initial commit

---
+++ src/main/java/com/keremakkale/entities/Student.java
@@ -0,0 +1,4 @@
+package com.keremakkale.entities;
+
+public class Student {
+}
---
+++ src/test/java/com/keremakkale/starter/SpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keremakkale.springdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
