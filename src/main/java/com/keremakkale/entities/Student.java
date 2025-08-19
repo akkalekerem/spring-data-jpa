bef3ae1bcc968fa55dd2620687d9ed6a186543d5
Kerem Akkale <keremakkale@gmail.com>
2025-08-19 16:08:33 +0300

    Primary key olan @Id'mizin Integer olması gerekiyormuş. @EntityScan eklendi. application.properties oluşturuldu.

--- src/main/java/com/keremakkale/entities/Student.java
+++ src/main/java/com/keremakkale/entities/Student.java
@@ -17,7 +17,7 @@ public class Student {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Integer id;
 
     @Column(name = "first_name", nullable = false, length = 40)
     private String firstName;
--- src/main/java/com/keremakkale/starter/SpringDataJpaApplication.java
+++ src/main/java/com/keremakkale/starter/SpringDataJpaApplication.java
@@ -2,7 +2,9 @@ package com.keremakkale.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan(basePackages = {"com.keremakkale"})
 @SpringBootApplication
 public class SpringDataJpaApplication {
 
